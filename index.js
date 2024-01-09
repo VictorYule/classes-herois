13519fce6374825a699a10e3d91f9e687679ed39
Victor Yule <105625896+VictorYule@users.noreply.github.com>
2024-01-09 05:55:55 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-first
+class Heroi {
+    constructor(nomeHeroi, idadeHeroi, classeHeroi) {
+      this.nomeHeroi = nomeHeroi;
+      this.idadeHeroi = idadeHeroi;
+      this.classeHeroi = classeHeroi;
+    }
+  
+    atacar() {
+      let ataque;
+  
+      switch (this.classeHeroi) {
+        case 'mago':
+          ataque = 'usou magia';
+          break;
+        case 'guerreiro':
+          ataque = 'usou espada';
+          break;
+        case 'monge':
+          ataque = 'usou artes marciais';
+          break;
+        case 'ninja':
+          ataque = 'usou shuriken';
+          break;
+        
+      }
+  
+      console.log(`O ${this.classeHeroi} atacou usando ${ataque}`);
+    }
+  }
+  
+  // Exemplos de uso da classe
+  const heroiGuerreiro = new Heroi('Hércules', 25, 'guerreiro');
+  heroiGuerreiro.atacar();
+  
+  const heroiMago = new Heroi('Magnus', 70, 'mago');
+  heroiMago.atacar();
+  
+  const heroiMonge = new Heroi('Shaolin', 63, 'monge');
+  heroiMonge.atacar();
+
+  const heroiNinja = new Heroi('Naruto', 18, 'ninja');
+  heroiNinja.atacar();
+  
